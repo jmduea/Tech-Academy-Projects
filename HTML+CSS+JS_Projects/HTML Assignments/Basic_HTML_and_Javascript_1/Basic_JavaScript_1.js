e76dbfdce3e2653875b63f267d060dec1b15b52c
jmduea <119540897+jmduea@users.noreply.github.com>
2022-12-07 15:01:30 -0600

    Moved project files around

---
+++ HTML+CSS+JS_Projects/HTML Assignments/Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+<script>
+alert("Welcome to my basic html and javascript page");
+</script>
+
+<script>
+	function My_First_Function() {
+		var string = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementByID("Irish").innerHTML =
+		result;
+	}
+</script>
