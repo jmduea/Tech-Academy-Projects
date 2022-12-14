e34362e6054784def5f46a1103fff26bdaf0e18c
jmduea <119540897+jmduea@users.noreply.github.com>
2022-12-14 08:33:31 -0600

    updated main.js

--- JavaScript Projects/Basic JavaScript Projects/Project2_functions/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -8,3 +8,17 @@ function My_Second_Function() {
     var result = Sent1.concat(Sent2); //this adds the two strings above into 1 variable
     document.getElementById("Button_Text").innerHTML = result; //this outputs the value of variable result when the button is pressed.
 }
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}   // this function assigns a string to the variable sentence and then adds another string onto it.
+    //on click it will output the value stored in the sentence variable.
+
+function my_Third_Function() {
+    var Sent3 = "This is my third function";
+    Sent3 += "; JavaScript Rocks!";
+    document.getElementById("Function3").innerHTML = Sent3;
+} //assigns a string to variable Sent3 and then adds on an other string.
+//on click it will return the value stored in variable Sent3 to the html element Function3
